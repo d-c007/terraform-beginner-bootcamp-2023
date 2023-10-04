0ec5dc00bbd15876122578d5bd748abbed81bc3b
d-c007 <129811195+d-c007@users.noreply.github.com>
2023-10-03 21:04:31 -0500

    23 re-structure root module  also added tf files and add some variables (#24)

--- main.tf
+++ main.tf
@@ -1,38 +1,18 @@
-terraform {
-  cloud {
-    organization = "Terraform-Beginner-Bootcamp-001"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-}
-
-provider "aws" {  
-}
-provider "random" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
-  length           = 32
-  special          = false
+  length   = 32
+  special  = false
 }
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
-    # Bucket Naming Rules
-    # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = random_string.bucket_name.result
-}
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  #bucket = random_string.bucket_name.result
 
-output "random_bucket_name_id" {
-  value = random_string.bucket_name.id
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
