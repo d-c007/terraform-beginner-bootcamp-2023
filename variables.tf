9239f837e1cd238a148e161e77e0066c4d56b4f3
d-c007 <129811195+d-c007@users.noreply.github.com>
2023-10-04 20:43:02 -0500

    # 27 added modules terrahouse folder and setup our terraform files appropriately (#28)
    We also verified everything worked huzzah

--- variables.tf
+++ variables.tf
@@ -1,18 +1,7 @@
 variable "user_uuid" {
-  description = "A UUID in string format"
-  type        = string
-  validation {
-    condition     = can(regex("^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})$", var.user_uuid))
-    error_message = "Invalid UUID format. Please provide a valid UUID."
-  }
+  type = string
 }
 
 variable "bucket_name" {
-  description = "AWS S3 Bucket Name"
-  type        = string
-
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "Invalid AWS S3 bucket name. Please provide a valid bucket name."
-  }
+  type = string
 }
