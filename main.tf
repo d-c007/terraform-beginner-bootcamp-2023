e05486e65880e03f1e484864d09bfe23f6c207ed
d-c007 <129811195+d-c007@users.noreply.github.com>
2023-10-25 14:06:14 -0500

    # 46 test out our terra home and validate that it works correctly also insured that we are in the correct town (#47)

--- main.tf
+++ main.tf
@@ -23,19 +23,19 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
   name = "Dune"
@@ -43,8 +43,8 @@ resource "terratowns_home" "home" {
   Dune is a visually stunning and epic science fiction film, directed by Denis Villeneuve and released in 2021. 
   It's based on the classic novel of the same name by Frank Herbert. The story takes place on the desert planet of Arrakis, the only source of a valuable spice called "melange." The film follows the journey of Paul Atreides, a young noble, as he becomes embroiled in a complex power struggle for control of Arrakis and its resources. With a star-studded cast, including Timothée Chalamet and Zendaya, "Dune" combines intricate world-building with themes of politics, religion, and destiny, resulting in a highly acclaimed and visually spectacular cinematic experience.
   DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "rivervalleybackroads.cloudfront.net"
   town = "video-valley"
   content_version = 1
 }
