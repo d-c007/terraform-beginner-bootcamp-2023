ef3d3c2ca5708df305c2d166628b412e6f8c7012
dakota copeland <dakota.copeland@yahoo.com>
2023-10-25 19:05:21 +0000

    # 46 test out our terra home and validate that it works correctly also insured that we are in the correct town

--- modules/terrahouse_aws/outputs.tf
+++ modules/terrahouse_aws/outputs.tf
@@ -1,14 +1,11 @@
-#output "bucket_name" {
-#  description = "Bucket name for our static website hosting"
-#  value = module.terrahouse_aws.bucket_name
-#}
-#
-#output "s3_website_endpoint" {
-#  description = "S3 Static Website hosting endpoint"
-#  value = module.terrahouse_aws.website_endpoint
-#}
-#
-#output "cloudfront_url" {
-#  description = "The CloudFront Distribution Domain Name"
-#  value = module.terrahouse_aws.cloudfront_url
-#}
+output "bucket_name" {
+  value = aws_s3_bucket.website_bucket.bucket
+}
+
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
